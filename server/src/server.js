eec927e5527b6624f13288d2b9730684a832f605
sachin624k <sachin77000k@gmail.com>
2026-07-04 03:16:33 +0530

    feat(auth): implement user registration and login with JWT

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,15 @@
-const app = require("./app");
+import dotenv from "dotenv";
+dotenv.config();
+
+import app from "./app.js";
+import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 8000;
 
+// Connect Database
+connectDB();
+
+// Start Server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
